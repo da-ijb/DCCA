f90435a85ac323fc9638873e8341ff0b2dd7f5aa
da-ijb <ndeiwmj237@icloud.com>
2020-11-04 17:59:33 +0900

    ログイン機能実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :user do
-    
+    transient do
+      person { Gimei.name }
+    end
+    nickname              { Faker::Name.name }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation {password} 
+    last_name             { person.last.kanji }
+    first_name            { person.first.kanji }
+    prefecture_id         { 2 }
+    introduction          { Faker::Lorem.sentence } 
   end
 end
